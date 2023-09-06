de3502ab77bb8b4d8416b22ea7a3a17283fe1ea3
tiravat thaisubvorakul <tiravat2016@gmail.com>
2023-09-07 02:42:55 +0700

    test card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import * as React from "react";
 
 export const Button = (props: any) => {
   const { label } = props;
-  return <button {...props} className="std-button">{label.toUpperCase()}</button>;
+  return (
+    <button {...props} className="std-button">
+      {label.toUpperCase()}
+    </button>
+  );
 };
 
 export const Card = ({ children }: { children: React.ReactNode }) => (
-  <div className="shadow-lg w-[400px] h-auto pt-6 pb-4 px-4 bg-white rounded-lg my-3 mx-1">
+  <div className="text-left shadow-lg w-[400px] h-auto p-4 bg-white rounded-lg my-3 mx-1">
     {children}
   </div>
 );
@@ -15,8 +19,8 @@ const App = () => {
   return (
     <div className="flex flex-col items-center justify-around w-full min-h-[1000px] py-1 px-3 m-0 top-0 left-0">
       <Card>
-        <h1 className="text-2xl font-bold text-gray-600 m-0">Information</h1>
-        <p className="mt-4 mb-5 text-base font-normal text-gray-700">
+        <h1 className="text-2xl font-bold text-gray-600 mx-0 mt-3 mb-0">Information</h1>
+        <p className="mt-4 mb-5 text-left font-normal text-gray-700">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, odio
           quasi repudiandae cum necessitatibus exercitationem animi, velit optio
           sit est voluptates pariatur beatae! Molestiae odio sed quo molestias.
@@ -27,6 +31,13 @@ const App = () => {
             <Button label="CANCEL" />
           </div>
         </div>
+      </Card>
+
+      <Card>
+        <Button label="CANCEL" />
+        <Button label="CANCEL" />
+        <Button label="CANCEL" />
+        <Button label="CANCEL" />
       </Card>
 
       <h1 className="my-4 text-2xl font-medium text-white">Hello</h1>
