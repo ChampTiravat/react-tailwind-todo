ee823a2a5773ca2196e5f88cc89d6caa468e962f
tiravat thaisubvorakul <tiravat2016@gmail.com>
2023-09-08 03:34:05 +0700

    feature: added form elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 
 export const Button = (props: any) => {
-  const { label, ...otherProps }: {label: string} = props;
+  const {
+    label,
+    fullWidth,
+    ...otherProps
+  }: { label: string; fullWidth: boolean } = props;
   return (
-    <button {...otherProps} className="std-button">
+    <button {...otherProps} className={`std-button ${fullWidth && "w-full mx-0"}`}>
       {label.toUpperCase()}
     </button>
   );
@@ -19,7 +23,43 @@ const App = () => {
   return (
     <div className="flex flex-col items-center justify-around w-full min-h-[1000px] py-1 px-3 m-0 top-0 left-0">
       <Card>
-        <h1 className="text-2xl font-bold text-gray-600 mx-0 mt-3 mb-0">Information</h1>
+        <h1 className="text-center text-2xl font-bold  mx-0 mt-3 mb-3">
+          Information
+        </h1>
+        <form
+          className="myy-2"
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("form submited")
+          }}
+        >
+          <div className="mx-0 my-3">
+            <label className="text-sm text-gray-500" htmlFor="A1">
+              Username
+            </label>
+            <input
+              className="w-full text-gray-500 rounded-md bg-gray-100 my-1 mx-0 py-2 px-4 border-[2px] border-gray-300"
+              id="A1"
+              type="text"
+            />
+          </div>
+          <div className="mx-0 my-3">
+            <label className="text-sm text-gray-500" htmlFor="A2">
+              Password
+            </label>
+            <input
+              className="w-full text-gray-500 rounded-md bg-gray-100 my-1 mx-0 py-2 px-4 border-[2px] border-gray-300"
+              id="A2"
+              type="password"
+            />
+          </div>
+          <Button type="submit" fullWidth label="SUBMIT" />
+        </form>
+      </Card>
+      <Card>
+        <h1 className="text-2xl font-bold text-gray-600 mx-0 mt-3 mb-0">
+          Information
+        </h1>
         <p className="mt-4 mb-5 text-left font-normal text-gray-700">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, odio
           quasi repudiandae cum necessitatibus exercitationem animi, velit optio
@@ -46,10 +86,6 @@ const App = () => {
         <Button label="ADD" />
         <Button label="ADD" />
       </div>
-      <Button label="Download" />
-      <Button label="Sign Me Up" />
-      <Button label="Toogle Block Comment" />
-      <Button label="Automatically Check for Updates" />
     </div>
   );
 };
